15c503dbe3262f6dd950f109a02a1cc3cd3e6533
Patrick Young <patrickyoung281@gmail.com>
2022-05-24 17:00:58 -0400

    commit tree 9be4f0e7d0ecc1d1dec31b7e83bf4bf747bf3978

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code goes here
+
+
+
+document.addEventListener("DomContentLoaded", function () {
+    const input = document.getElementById("text")
+    input.textContent = "This is really cool!"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+document.addEventListener("DOMContentLoaded", function () {
+    const p = document.getElementById("text")
+    p.textContent = "This is really cool!"
+});
+
+*/
